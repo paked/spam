e9a05176223546ad3b510ca0d01458cd2dd552cb
Harrison <harrison@theshoebridges.com>
2016-01-29 23:29:54 +1100

    Add non determinism

--- cmd/spam/spam.go
+++ cmd/spam/spam.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/paked/spam"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	s := spam.New(os.Stdin)
 
 	fmt.Print(s)
